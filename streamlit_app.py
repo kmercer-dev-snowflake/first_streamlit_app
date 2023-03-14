2a990ebf1ce7383cd021b57b72a01d27c3da10b2
kmercer-dev-snowflake <127784073+kmercer-dev-snowflake@users.noreply.github.com>
2023-03-14 08:38:19 -0400

    Update streamlit_app.py
    changed up the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
-streamlit.title('My Parents Healthy New Dinner')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range egg')
+streamlit.title('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range egg')
+streamlit.text('🥑🍞 Avocado Toast')
