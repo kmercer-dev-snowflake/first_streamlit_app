9f86d474215b3c9d6f763f10dc5aa25ab8440a25
kmercer-dev-snowflake <127784073+kmercer-dev-snowflake@users.noreply.github.com>
2023-03-21 14:19:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Displays the response in a nice little table, everybody can use a nice little table
 streamlit.dataframe(fruityvice_normalized)
 
+# don't run anything past here while we troubleshoot
+streamlit.stop()
 
 import snowflake.connector 
 
