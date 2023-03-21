4a0cbb1ce4261f5a4642a9b8434e62403b3d6b57
kmercer-dev-snowflake <127784073+kmercer-dev-snowflake@users.noreply.github.com>
2023-03-21 14:48:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,8 +54,7 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-# don't run anything past here while we troubleshoot
-streamlit.stop()
+
 
 # allow the end user to add a fruit to the list 
 def insert_row_snowflake(new_fruit):
@@ -69,7 +68,8 @@ if streamlit.button('Add a Fruit to the list'):
   back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
 
-
+# don't run anything past here while we troubleshoot
+streamlit.stop()
 
 #streamlit.write('The user entered ', add_my_fruit)
 
